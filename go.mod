cf914ac1bcfcca2bafb5ea780afc8c7f32282111
Vítor Nunes <vitor.sobrinho.nunes@gmail.com>
2026-01-07 21:56:46 +0000

    Add initial server WebSocket reader

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module ws-chat/server
 
 go 1.25.5
+
+require github.com/gorilla/websocket v1.5.3 // indirect
--- server/server.go
+++ server/server.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+func HandleNewConnection(writer http.ResponseWriter, request *http.Request) {
+	// Send HTTP upgrade to upgrade HTTP to WebSockets
+	var upgrader = websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
+
+	ws, err := upgrader.Upgrade(writer, request, nil)
+	if err != nil {
+		fmt.Printf("Failed to upgrade HTTP connection to WS: %s\n", err)
+		return
+	}
+
+	for {
+		_, message, err := ws.ReadMessage()
+		if err != nil {
+			fmt.Printf("ReadMessage failed: %s\n", err)
+			break
+		}
+		fmt.Printf("Received: %s\n", message)
+	}
+}
 
 func main() {
+	var port = 8080
 	fmt.Println("ws-chat server")
+	http.HandleFunc("/ws", HandleNewConnection)
+
+	fmt.Printf("Server listening on port %d\n", port)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	if err != nil {
+		fmt.Printf("Failed to bind address: %s\n", err)
+	}
 }
